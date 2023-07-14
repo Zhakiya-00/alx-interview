2289b7399cdd3920834a29a9f14fca505b11a1ff
root <zakiyayusuf123@gmail.com>
2023-07-14 10:28:28 +0000

    main

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -12,5 +12,5 @@ def print_triangle(triangle):
         print("[{}]".format(",".join([str(x) for x in row])))
 
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
