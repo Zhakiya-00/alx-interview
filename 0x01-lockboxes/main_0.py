24578c9ddf392802415a5372a188788e33040a2c
root <zakiyayusuf123@gmail.com>
2023-07-14 11:30:34 +0000

    PEP8 correction

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -11,4 +11,4 @@ print(canUnlockAll(boxes))
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
 
-carrie@ubuntu:~/0x01-lockboxes$
+carrie@ubuntu: ~/0x01-lockboxes$
