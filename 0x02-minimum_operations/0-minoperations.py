15dd4002d4f55fd34b9134aa3c6b90a3da5368d5
Zhakiya-00 <themaverick@DESKTOP-NANNRMQ.localdomain>
2023-07-16 21:28:41 +0300

    minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
 
-"""return an integer"""
+""" Minimum Operations """
 
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
-    Args:
-        n (int): The desired number of H characters.
-
-    Returns:
-        int: The fewest number of operations needed.
-
+    Returns an integer
+    If n is impossible to achieve, returns 0
     """
-
-    # If n is less than 2, it is impossible to achieve
-    if n < 2:
+    if not isinstance(n, int):
         return 0
 
-    # Initialize the number of operations and the clipboard with 1 H character
-    num_operations = 0
-    clipboard = 1
-
-    # Start with 2 H characters already in the file
-    file_content = 2
-
-    while file_content < n:
-        if n % file_content == 0:
-            # If the current file content is a factor of n, we can Copy All and Paste multiple times
-            num_paste_operations = n // file_content - 1
-            num_operations += num_paste_operations + 1
-            clipboard = file_content
-        else:
-            # If the current file content is not a factor of n, we can only Paste
-            num_operations += 1
-            file_content += clipboard
-
-    return num_operations 
+    operations = 0
+    num_chars = 2
+    while (num_chars <= n):
+        if not (n % num_chars):
+            n = int(n / num_chars)
+            operations += num_chars
+            num_chars = 1
+        num_chars += 1
+    return operations 
