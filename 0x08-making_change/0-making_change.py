52e8c99091e6ad19ce01128184f9e2f5644dd45c
Zhakiya-00 <themaverick@DESKTOP-NANNRMQ.localdomain>
2023-09-18 21:01:54 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Change comes from within
+Task: Change comes from within
 """
 
 
@@ -8,26 +8,31 @@ def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest
     number of coins needed to meet a given amount total.
-
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
     if total <= 0:
         return 0
 
-    placeholder = total + 1
-
-    memo = {0: 0}
+    newVal = total + 1
+    store = {0: 0}
 
     for i in range(1, total + 1):
-        memo[i] = placeholder
+        store[i] = newVal
 
         for coin in coins:
             current = i - coin
             if current < 0:
                 continue
 
-            memo[i] = min(memo[current] + 1, memo[i])
+            store[i] = min(store[current] + 1, store[i])
 
-    if memo[total] == total + 1:
+    if store[total] == total + 1:
         return -1
 
-    return memo[total]
+    return store[total]
